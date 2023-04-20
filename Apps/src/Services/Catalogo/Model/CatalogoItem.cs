840e3f0bf62421dcbad5aaa13e01ab9573e3ad35
phillrog <phillrog@hotmail.com>
2023-04-19 21:07:31 -0300

    criação do model

---
+++ Apps/src/Services/Catalogo/Model/CatalogoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalogo.API.Model
+{
+    public class CatalogoItem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public CatalogoItem() {}
+    }
+}
