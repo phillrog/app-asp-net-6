3a27f5929d6319d8ac76502f7dc418d98c94b3d4
phillrog <phillrog@hotmail.com>
2023-04-19 21:07:16 -0300

    criação do catalogo context

---
+++ Apps/src/Services/Catalogo/Infrastructure/CatalogoContext.cs
@@ -0,0 +1,12 @@
+﻿using Catalogo.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.Infrastructure
+{
+    public class CatalogoContext : DbContext
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
+
+        public DbSet<CatalogoItem> CatalogoItems { get; set; }
+    }
+}
