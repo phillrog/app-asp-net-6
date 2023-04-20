5f7e48864b96694254c4714b40087fd2fd56f71a
phillrog <phillrog@hotmail.com>
2023-04-20 19:26:24 -0300

    configurado keycloak na api

--- Apps/src/Services/Catalogo/Program.cs
+++ Apps/src/Services/Catalogo/Program.cs
@@ -1,4 +1,7 @@
 using Catalogo.Infrastructure;
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Sdk.Admin;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -9,9 +12,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var authenticationOptions = builder
+                            .Configuration
+                            .GetSection(KeycloakAuthenticationOptions.Section)
+                            .Get<KeycloakAuthenticationOptions>();
+
+builder.Services.AddKeycloakAuthentication(authenticationOptions);
+
+var authorizationOptions = builder
+                            .Configuration
+                            .GetSection(KeycloakProtectionClientOptions.Section)
+                            .Get<KeycloakProtectionClientOptions>();
+
+builder.Services.AddKeycloakAuthorization(authorizationOptions);
+
+var adminClientOptions = builder
+                            .Configuration
+                            .GetSection(KeycloakAdminClientOptions.Section)
+                            .Get<KeycloakAdminClientOptions>();
+
+builder.Services.AddKeycloakAdminHttpClient(adminClientOptions);
+
 
 var app = builder.Build();
 
@@ -24,6 +49,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
