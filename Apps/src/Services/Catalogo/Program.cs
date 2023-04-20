4ba4b05927fc3a9cdcc136c98002e734f8c37525
phillrog <phillrog@hotmail.com>
2023-04-19 21:42:31 -0300

    configuração do contexto

--- Apps/src/Services/Catalogo/Program.cs
+++ Apps/src/Services/Catalogo/Program.cs
@@ -1,6 +1,12 @@
+using Catalogo.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,3 +29,24 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public static class CustomExtensionMethods
+{
+    public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEntityFrameworkSqlServer()
+            .AddDbContext<CatalogoContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("Localhost"),
+                                        sqlServerOptionsAction: sqlOptions =>
+                                        {
+                                            sqlOptions.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+                                            sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                                        });
+            });
+       
+
+        return services;
+    }
+
+}
